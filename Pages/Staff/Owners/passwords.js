d55d6f818ff6dafe6198ae4eb0042943cd9d1494
MatthiasMortimer <163493787+MatthiasMortimer@users.noreply.github.com>
2025-06-24 18:58:33 -0400

    Update passwords.js
    Added password for Owners page shortcut

--- Pages/Staff/Owners/passwords.js
+++ Pages/Staff/Owners/passwords.js
@@ -1,2 +1,3 @@
 // All protected passwords go here
 const password1a = "mySecretPassword"; // This is for the Staff page.
+const password1b = "letmein"; // ← This is for the shortcut to the Owners page from Home.
