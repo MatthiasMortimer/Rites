9bef17106a2586763ee7a1bacba481172a0d930b
MatthiasMortimer <163493787+MatthiasMortimer@users.noreply.github.com>
2025-07-02 12:12:25 -0400

    Fixed the password file

--- Pages/Staff/Owners/passwords.js
+++ Pages/Staff/Owners/passwords.js
@@ -1,3 +1,3 @@
 // All protected passwords go here
-const password1a = "ownerPass"; // This is for the Staff page.
-const password1b = "letmein"; // ← This is for the shortcut to the Owners page from Home.
+const password1a = "staffletmein"; // This is for the Staff page.
+const password1b = "ownerpass"; // ← This is for the shortcut to the Owners page from Home.
